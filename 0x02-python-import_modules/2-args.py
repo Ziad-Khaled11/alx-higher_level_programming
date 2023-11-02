575014d0ba533e1523ca7a7c4f59f21317b74db1
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-11-02 22:21:01 +0200

    Mango ;)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
     index = 1
     if co == 1:
         print("{} arguments.".format(co - 1))
+    elif co == 2:
+        print("{} arguments:".format(co - 1))
     else:
         print("{} arguments:".format(co - 1))
-        while co - 1 > 0:
-            print("{}: {}".format(index, argv[index]))
-            index += 1
-            co -= 1
+    while co - 1 > 0:
+        print("{}: {}".format(index, argv[index]))
+        index += 1
+        co -= 1
