1400cf4b303484e08390f11356d67ee4ca923468
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-11-02 22:26:37 +0200

    Mango ;)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,11 @@
 if __name__ == '__main__':
     import sys
     co = len(sys.argv)
-    index = 1
     if co == 1:
         print("{} arguments.".format(co - 1))
     elif co == 2:
         print("{} arguments:".format(co - 1))
     else:
         print("{} arguments:".format(co - 1))
-    while co - 1 > 0:
-        print("{}: {}".format(index, sys.argv[index]))
-        index += 1
-        co -= 1
+    for i in range(1, co):
+        print("{}: {}".format(i, sys.argv[i]))
