95af1e0aa30f3170ed2ff3aca8b7c65e600ff90c
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-11-10 06:24:25 +0200

    Mango ;)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,13 @@
 def roman_to_int(roman_string):
     dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if roman_string and isinstance(roman_string, str):
-        Sum = 0
-        for i in roman_string:
-            for key, value in dic.items():
-                if i == key:
-                    Sum += value
-        return Sum
+        numbers = [dic[x] for x in roman_string] + [0]
+        rep = 0
+        for i in range(len(numbers) - 1):
+            if numbers[i] >= numbers[i+1]:
+                rep += numbers[i]
+            else:
+                rep -= numbers[i]
+        return rep
     else:
         return 0
