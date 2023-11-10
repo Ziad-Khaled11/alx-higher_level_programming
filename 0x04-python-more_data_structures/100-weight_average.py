8189186b68dfac90eace1dc06d8bd14e1f9cef63
Ziad-Khaled11 <ziadshraf100@gmail.com>
2023-11-10 05:53:31 +0200

    Mango ;)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    Sum = 0
+    quot = 0
+    for i in my_list:
+        Sum += i[0] * i[1]
+        quot += i[1]
+    averge = Sum / quot
+    return averge
