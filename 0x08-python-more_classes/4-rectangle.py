8ad874173916d52ac4d73a084c8b38a975fc8b0f
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-02-24 22:15:49 +0200

    Mango ;)

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,7 +67,7 @@ class Rectangle:
                 if i != self.__height - 1:
                     rect.append("\n")
             return ("".join(rect))
-    
+
     def __repr__(self):
         """Return a string representation of this object as a string."""
         return (f"Rectangle({self.__width}, {self.__height})")
