8b73270215b8931f201b834138ab4eda801b9f9b
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-02-24 22:34:46 +0200

    Mango ;)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -85,5 +85,5 @@ class Rectangle:
         """Deletes this object
             and decrements the number of instances
         """
-        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
+        Rectangle.number_of_instances = Rectangle.number_of_instances - 1
         print("Bye rectangle...")
