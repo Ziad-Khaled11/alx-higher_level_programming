29d4170f61c83708ef83071f21dcdd4ebfdfca0a
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-02-24 19:24:04 +0200

    Mango ;)

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""This is a Rectangle blueprint class"""
+class Rectangle:
+    """This is a Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+This is a Python OOP Classes Tasks
+which covers some dunder methods and dive into class attributes
