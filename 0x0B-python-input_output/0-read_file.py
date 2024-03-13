ff84701c9fd1ca2e8cefb8ffcb38fa00ac799bda
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-03-13 22:50:56 +0200

    Mango ;)

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Function Reads from a file
+    """
+    with open(filename, "r") as f:
+        line = f.readline()
+        while line:
+            print(line.strip())
+            line = f.readline()
