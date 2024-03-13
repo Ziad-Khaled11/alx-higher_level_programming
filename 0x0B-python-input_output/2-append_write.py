30eb8b9a8d1211e13129a7411ae52be1c57526cf
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-03-13 23:29:10 +0200

    Mango ;)

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function appends
+"""
+def append_write(filename="", text=""):
+    """
+    appends a given text in a file
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
