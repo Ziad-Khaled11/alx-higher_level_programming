c3eaa769b1c10bcb7354b7c477fe2a12b4b98a59
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-06-21 02:54:27 +0300

    Mango ;)

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,9 +4,6 @@ const arg = Number(process.argv[2]);
 if (isNaN(arg)) { console.log('Missing size'); } else {
   const size = Number(process.argv[2]);
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      console.log('X');
-    }
-    console.log();
+    console.log('X'.repeat(size));
   }
 }
