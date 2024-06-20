05ed17197fdbffca0809fa5a4a45d223895dc989
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-06-21 02:38:57 +0300

    Mango ;)

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* prints strings from an array */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
