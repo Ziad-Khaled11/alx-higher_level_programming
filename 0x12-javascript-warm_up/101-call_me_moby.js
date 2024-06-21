d0f81e883b9fea64545711a1bfecf97d42a0c812
Ziad-Khaled11 <ziadshraf100@gmail.com>
2024-06-21 04:10:40 +0300

    Mango ;)

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* js */
-exports.callMeMoby = function (x, the Function) {
-for (let i=0; i < x; i++) {
-the Function();
-}
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
